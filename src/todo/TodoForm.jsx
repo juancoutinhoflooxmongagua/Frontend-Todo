70a31bcb91cdabbdce1fdeb352734573e466d6ff
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-04 12:39:05 -0300

    feat: add buttons and add events

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const TodoForm = props => (
-    <div>
-        <h1>Form</h1>
+    <div role='form' className='row my-3'>
+      
+        <div className="col-9">
+            <input 
+                type="text" 
+                id='desc' 
+                placeholder='Adicionar tarefa...' 
+                className="form-control"
+            /> 
+        </div>
+
+        <div className="col-3 d-flex justify-content-end">
+            <button onClick={props.handleAdd} className="btn btn-primary">
+                <i className="fa fa-plus"></i> Adicionar
+            </button>
+        </div>
     </div>
 )
 
