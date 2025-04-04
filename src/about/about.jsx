c5c5d4694ad66ea561cea5c7a2b607967b58131c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-04 12:34:28 -0300

    feat: add form and add list

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -3,6 +3,7 @@ import PageHeader from "../template/pageHeader";
 export default props => (
     <div>
         <PageHeader name='Sobre'/>
+        <p>Lorem ipsum dolor sit amet. conseqtuor asdicep adilian</p>
     </div>
  )
  
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from "react";
 import PageHeader from "../template/pageHeader";
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
 export default class Todo extends Component {
     render() {
         return (
             <div>
                <PageHeader name='Tarefas'/>
+               <TodoForm />
+               <TodoList />
             </div>
         )
     }
